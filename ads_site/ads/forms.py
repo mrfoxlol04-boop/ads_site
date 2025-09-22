c1fee62ef9bd61c3499cfa82cbe025f35e3ea1d4
mrfoxlol04-boop <mr.foxlol04@gmail.com>
2025-09-22 13:05:36 +0400

    Delete ads_site directory

--- ads_site/ads/forms.py
+++
@@ -1,9 +0,0 @@
-# ads/forms.py
-
-from django import forms
-from .models import Advertisement
-
-class AdvertisementForm(forms.ModelForm):
-    class Meta:
-        model = Advertisement
-        fields = ['title', 'short_description', 'full_description']
--- ads_site/ads_site/urls.py
+++
@@ -1,9 +0,0 @@
-# ads_site/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ads.urls')),
-]
